ebf662a3e6ab51cb6a67dd1b497d8a8a549f6959
saran858 <saranacds@gmail.com>
2024-06-29 10:11:12 +0530

    fix: resolved cors

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ app.use(express.json());
 connectDB();
 
 const PORT = 3500;
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("Hello Geeks");
