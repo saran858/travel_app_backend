7469a40ce65ccb910561fb1f58244b8c73979fb9
saran858 <saranacds@gmail.com>
2024-06-29 11:59:29 +0530

    fix: resolved cors

--- server.js
+++ server.js
@@ -18,23 +18,16 @@ const connectDB = require("./config/dbconfig");
 
 const app = express();
 
-const corsOptions = {
-  origin: "*", // or your frontend domain
-  optionsSuccessStatus: 200 // For legacy browser support
-};
-
-app.use(cors(corsOptions));
+app.use(
+cors({
+  origin: "*",})
+);
 app.use(express.json());
 connectDB();
 
 const PORT = 3500;
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', "*"); // or your frontend domain
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+
 
 app.get("/", (req, res) => {
   res.send("Hello Geeks");
